d782920f704a0c2f7b7dc744b9c0f86a9b08ff2a
hoseong <43123909+hoseong85@users.noreply.github.com>
2023-03-04 00:59:39 +0900

    #2.1 Mouse Painting

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
+let isPainting = false;
 
-const colors = [
-    "#1abc9c",
-    "#f1c40f",
-    "#3498db",
-    "#e74c3c",
-];
-
-function onclick(event){
-    ctx.beginPath();
-    ctx.moveTo(400, 400);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        // ctx.fill();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting() {
+    isPainting = true;
+}
+function cancelPainting() {
+    isPainting = false;
 }
 
-canvas.addEventListener("mousemove", onclick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+// document.addEventListener("mouseup", cancelPainting);
