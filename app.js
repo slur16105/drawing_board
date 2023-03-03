67d6c2b49ebbc14702ef2969f1bdd58d31e8c519
hoseong <43123909+hoseong85@users.noreply.github.com>
2023-03-04 00:43:56 +0900

    #2.0 Painting Lines

--- app.js
+++ app.js
@@ -2,5 +2,22 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-ctx.fillRect(50, 50, 100, 200);
+const colors = [
+    "#1abc9c",
+    "#f1c40f",
+    "#3498db",
+    "#e74c3c",
+];
+
+function onclick(event){
+    ctx.beginPath();
+    ctx.moveTo(400, 400);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onclick);
